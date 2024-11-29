0bd24e2b91899820ae45af5f194f24d22cc1b927
DiegoEspinosaM02 <dieguindem@gmail.com>
2024-11-29 11:05:12 -0600

    rutas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,28 @@
 <?php
 
 use App\Http\Controllers\SombreroController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/sombreros',[SombreroController::class, 'index']);
+Route::get('/usuario', [UsuarioController::class, 'index']);
+
+Route::get('/usuario', [UsuarioController::class, 'index']);
+
+
+Route::post('/usuario/create', [UsuarioController::class, 'create']);
+
+
+Route::post('/usuario', [UsuarioController::class, 'store']);
+
+
+Route::get('/usuario/{id}', [UsuarioController::class, 'show']);
+
+
+Route::get('/usuario/{id}/edit', [UsuarioController::class, 'edit']);
+
+
+Route::put('/usuario/{id}', [UsuarioController::class, 'update']);
+Route::patch('/usuario/{id}', [UsuarioController::class, 'update']); // PATCH como alternativa
+
+
+Route::delete('/usuario/{id}', [UsuarioController::class, 'destroy']);
